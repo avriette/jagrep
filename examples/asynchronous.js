97008f9c1b07c19ba8723b58647d16264276e90c
jane avriette <jane@cpan.org>
2014-11-02 13:42:49 -0500

    resolves issue #3

--- examples/asynchronous.js
+++ examples/asynchronous.js
@@ -1,19 +1,41 @@
 #!/usr/bin/env node
 
+/*
+
+  Example for asynchronous usage of jagrep
+
+*/
 var Jgrep = require( 'jagrep' );
 
 var test    = [ 'test', 'another test', 1234, 'a string' ]
 	, promise = undefined;
+
+/*
+  As with sync(), the first argument provided to async() should be a hash
+  with either 'function' or 'expression' as a key. The return value will
+  be a promise of a list that will be complete when the operation is
+  complete.
+*/
 
 promise = Jgrep.async(
 	{ 'expression': new RegExp( '^test$' ) }
 	, test
 );
 
+/*
+  See the documentation for q for more information on promises.
+*/
+
 promise.then( console.log );
 
 promise = undefined;
 
+/*
+  If a function is supplied as the third argument to async(), it will be
+  called with a promise to the results of the operation. As with sync(),
+  the supplied test function will be called for each element in the supplied
+  list.
+*/
 var rval = Jgrep.async(
 	{ 'function' : function (t) { if ( t == 'test' ) return 1 } }
 	, test
--- examples/synchronous.js
+++ examples/synchronous.js
@@ -1,10 +1,25 @@
 #!/usr/bin/env node
 
-var Jgrep = require( 'jagrep.js' );
+/*
+
+  Example for synchronous usage of jagrep
+
+*/
+
+var Jgrep = require( 'jagrep' );
 
 var test    = [ 'test', 'another test', 1234, 'a string' ]
 	, results = [ ];
 
+/*
+  The first argument provided to sync() should be a hash with either
+  'function' or 'expression' as a key.
+*/
+
+/*
+  With a supplied expression, the returned list will be all elements which
+  match the supplied expression.
+*/
 results = Jgrep.sync(
 	{ 'expression': new RegExp( '^test$' ) }
 	, test
@@ -14,6 +29,11 @@ console.log( results );
 
 results = [ ];
 
+/*
+  With a supplied function, the returned list will be all elements for which
+  the function supplied returned true, given each individual element of the
+  supplied list.
+*/
 results = Jgrep.sync(
 	{ 'function': function (t) { if ( t == 'test' ) return 1 } }
 	, test

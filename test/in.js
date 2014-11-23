90539c375f79aa872d5667373fda6d832a0ab5ea
jane avriette <jane@cpan.org>
2014-11-23 15:32:51 -0500

    test for all_in fix all_in (missing list parameter in sync())

--- test/in.js
+++ test/in.js
@@ -4,9 +4,22 @@ var assert  = require('assert')
 
 var input    = [ 'test', 'another test', 1234, 'a string' ];
 
-it('in: positive result', function () {
+it('in', function () {
 	var r = Jgrep.in( input, 'a string' );
 	assert( r, 'returned truth' );
 	var s = Jgrep.in( input, 'peace on earth' );
 	chai.assert.notOk( s, 'returned false (boo-hoo)' );
+} );
+
+it('all_in', function () {
+	var r = Jgrep.all_in( input, [ 'a string', 'test' ] );
+	assert(r, 'truth returned');
+
+	// We do not guarantee the return order of lists. Chai has this cool 'equal-ish' assert:
+	//   .include(haystack, needle, [message])
+	//
+
+	r.forEach( function (test) {
+		chai.assert.include( [ 'a string', 'test' ], test, 'correct values returned' );
+	} );
 } );
